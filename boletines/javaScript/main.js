3090afcaf46622caab52e679495d250de8471434
Mateo Sanchez <mateosanchez9602@gmail.com>
2020-04-09 16:38:32 -0500

    Eventos javaScript con la clase css close

--- boletines/javaScript/main.js
+++ boletines/javaScript/main.js
@@ -2,10 +2,33 @@
 
 //DOM
 //querySelectorAll / querySelector
+
+/*
 let links = document.querySelectorAll("a");
 
 links.forEach(function(link) {
     console.log(link)
 })
+*/
 
 //console.log(links);
+
+/*
+let td = document.querySelectorAll("td");
+
+td.forEach(function(td) {
+    td.addEventListener('click', function() {
+        console.log(this);
+    })
+})
+*/
+
+//Obtener los elementos de la clase .close del html sensei
+let links = document.querySelectorAll(".close");
+//Recorrelos
+links.forEach(function(cls) {
+    //Agregar un evento click en cada uno de ellos
+    cls.addEventListener('click', function() {
+        console.log(":)");
+    });
+});
